11dc5727383c30bb74355723ca642598242a346f
JayMcDowell <jay@musicianshalloffame.com>
2013-04-29 11:09:32 -0500

    integrated leap year test

--- cal.rb
+++ cal.rb
@@ -1,5 +1,35 @@
-month = ARGV[0]
-year = ARGV[1]
+  month = ARGV[0]
+  year = ARGV[1]
 
-puts `cal #{month} #{year}`
+  puts `cal #{month} #{year}`
 
+class Calendar
+  # attr_accessor :month
+  attr_accessor :year
+
+
+  def initialize(year)
+    # @month = month
+    @year = year
+  end
+
+
+  # def year
+
+  # end
+
+
+
+  def leap_year?
+
+    if year % 400 == 0
+       true
+    elsif year % 4 == 0
+        true
+    elsif year % 100 == 0
+        false
+    else
+       false
+    end
+  end
+end
--- test/testCal.rb
+++ test/testCal.rb
@@ -1,4 +1,5 @@
 require 'test/unit'
+require './cal.rb'
 
 class CalTest < Test::Unit::TestCase
 
@@ -6,8 +7,19 @@ class CalTest < Test::Unit::TestCase
   assert_equal(`cal 2 2012`,`ruby cal.rb 2 2012`)
   end
 
+  def test_02_ruby_cal_can_find_if_it_is_leap_year
+    cal = Calendar.new(2012)
+    assert_equal(true, cal.leap_year?)
+  end
 
+    def test_02_ruby_cal_can_find_if_it_is_leap_year
+    cal = Calendar.new(2013)
+    assert_equal(false, cal.leap_year?)
+  end
 
-
+    def test_02_ruby_cal_can_find_if_it_is_leap_year
+    cal = Calendar.new(2000)
+    assert_equal(true, cal.leap_year?)
+  end
 
 end
