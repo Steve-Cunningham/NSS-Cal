5e924b9ca6c253b78ec49e37242d4a0a149061f0
JayMcDowell <jay@musicianshalloffame.com>
2013-04-30 14:53:44 -0500

    part 2

--- cal.rb
+++ cal.rb
@@ -1,27 +1,73 @@
   month = ARGV[0]
   year = ARGV[1]
 
-  puts `cal #{month} #{year}`
+  # puts `cal #{month} #{year}`
 
 class Calendar
-  # attr_accessor :month
+  attr_accessor :month
   attr_accessor :year
+  attr_accessor :months
 
-
-  def initialize(year)
-    # @month = month
+  def initialize(month, year)
+    @month = month
     @year = year
+    @months = months
   end
 
+  def bogus_month
 
-  # def year
+    if month >12
+      raise ArgumentError, "This Calendar only works with months between 1 and 12"
+    elsif month <1
+      raise ArgumentError, "This Calendar only works with months between 1 and 12"
+    end
+  end
 
-  # end
+  def month_to_s
+    if month.to_i > 0
+      months = ["January",
+                "February",
+                "March",
+                "April",
+                "May",
+                "June",
+                "July",
+                "August",
+                "September",
+                "October",
+                "November",
+                "December"]
+      month_to_s = months[month-1]
+    else
+    #   month.upcase!
+    #   months.upcase! == caps_month
+    #   month_to_s = months[month-1]
+    end
+  end
 
+  def bogus_year
+    if year <1800
+      raise ArgumentError, "This Calendar only works between 1800 and 3000"
+    elsif year >3000
+      raise ArgumentError, "This Calendar only works between 1800 and 3000"
+    end
+  end
 
+  def year_to_s
+    @year = year.to_s
+  end
+
+  def days_header
+    return "Su Mo Tu We Th Fr Sa"
+  end
+
+  def month_year_header
+    @year = year.to_s
+    header = "#{month_to_s} #{year_to_s}"
+    header.center(20)
+  end
 
   def leap_year?
-
     if year % 400 == 0
        true
     elsif year % 4 == 0
--- test/testCal.rb
+++ test/testCal.rb
@@ -3,23 +3,88 @@ require './cal.rb'
 
 class CalTest < Test::Unit::TestCase
 
-  def test_01_ruby_cal_works_with_cal
-  assert_equal(`cal 2 2012`,`ruby cal.rb 2 2012`)
-  end
+  # def test_01_ruby_cal_works_with_cal
+  # assert_equal(`cal 2 2012`,`ruby cal.rb 2 2012`)
+  # end
 
   def test_02_ruby_cal_can_find_if_it_is_leap_year
-    cal = Calendar.new(2012)
+    cal = Calendar.new(2, 2012)
     assert_equal(true, cal.leap_year?)
   end
 
-    def test_02_ruby_cal_can_find_if_it_is_leap_year
-    cal = Calendar.new(2013)
+    def test_03_ruby_cal_can_find_if_it_is_leap_year
+    cal = Calendar.new(2, 2013)
     assert_equal(false, cal.leap_year?)
   end
 
-    def test_02_ruby_cal_can_find_if_it_is_leap_year
-    cal = Calendar.new(2000)
+    def test_04_ruby_cal_can_find_if_it_is_leap_year
+    cal = Calendar.new(2, 2000)
     assert_equal(true, cal.leap_year?)
   end
 
+  def test_05_ruby_cal_works_with_cal
+    cal = Calendar.new(4, 2012)
+    assert_equal(cal.year, 2012)
+  end
+
+  def test_05_ruby_cal_works_with_cal
+    cal = Calendar.new(4, 2012)
+    assert_equal(cal.year, 2012)
+  end
+
+  def test_06_ruby_cal_gets_month_name_to_string
+    cal = Calendar.new(4, 2012)
+    assert_equal(cal.month_to_s, "April")
+  end
+
+  def test_06b_ruby_cal_cannot_accept_bogus_month
+    cal = Calendar.new(0, 2012)
+    assert_raise ArgumentError do
+        cal.bogus_month
+    end
+  end
+
+  # def test_06c_ruby_cal_cannot_accept_bogus_month
+  #   cal = Calendar.new("jan", 1800)
+  #   assert_equal(cal.month_to_s, "January")
+  # end
+
+  # def test_06d_ruby_cal_capitalization_matters
+  #   cal = Calendar.new("Jan", 1800)
+  #   assert_equal(cal.month_to_s, "January")
+  # end
+
+  def test_06e_ruby_cal_cannot_accept_bogus_year
+    cal = Calendar.new(6, 1799)
+    assert_raise ArgumentError do
+        cal.bogus_year
+    end
+  end
+
+  def test_07_ruby_cal_gets_year_to_string
+    cal = Calendar.new(4, 2012)
+    assert_equal(cal.year_to_s, "2012")
+  end
+
+  def test_07_ruby_cal_gets_month_and_year_to_header
+    cal = Calendar.new(4, 2012)
+    assert_equal(cal.month_year_header, "     April 2012     ")
+  end
+
+  def test_08_ruby_cal_gets_month_and_year_to_header_centered
+    cal = Calendar.new(6, 2012)
+    assert_equal(cal.month_year_header, "     June 2012      ")
+  end
+
+    def test_09_ruby_cal_puts_days_to_header
+    cal = Calendar.new(6, 2012)
+    assert_equal(cal.days_header, "Su Mo Tu We Th Fr Sa")
+  end
+
 end
+
+
+
+
+
+
